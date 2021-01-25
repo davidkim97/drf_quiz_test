c9cccf60a0742c2efeb445eb4c509b86702c52ca
davidkim97 <davidmim99@gmail.com>
2021-01-25 21:34:47 +0900

    Ready for deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicon myapi.wsgi
---
+++ quiz/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from .views import helloAPI, randomQuiz
+
+
+urlpatterns = [
+    path("hello/", helloAPI),
+    path("<int:id>/", randomQuiz),
+]
